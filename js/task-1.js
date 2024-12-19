ff9b6e47a09d82913fd0cd6a7d8eb4c89e03392d
polly <polifibliss@gmail.com>
2024-12-19 17:55:17 +0200

    update

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const categories = document.querySelectorAll('ul#categories > li.item');
+
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach((category) => {
+  const categoryTitle = category.querySelector('h2').textContent;
+
+  const elementsCount = category.querySelectorAll('ul > li').length;
+
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${elementsCount}`);
+});
